f7e2832dd190b199e918ddefbd515eb0eaa38f6f
I-Frostbyte <irefyre@gmail.com>
2026-07-01 18:52:26 +0100

    Adding dribble-assignment-1

---
+++ Projects/dribble-assignment-1/src/App.js
@@ -0,0 +1,15 @@
+
+import LeftSidebar from './components/LeftSidebar';
+import RightSidebar from './components/RightSidebar';
+// import RightSidebar from './components/RightSidebar';
+
+function App() {
+  return (
+    <div className='sidebars'>
+      <LeftSidebar />
+      <RightSidebar />
+    </div>
+  );
+}
+
+export default App;
