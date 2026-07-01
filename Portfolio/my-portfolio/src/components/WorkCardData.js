e0bedf7e7702c754bacbd1b452049b18c096f2aa
I-Frostbyte <irefyre@gmail.com>
2026-07-01 18:42:38 +0100

    Adding Portfolio

---
+++ Portfolio/my-portfolio/src/components/WorkCardData.js
@@ -0,0 +1,29 @@
+import pro1 from "../assets/myproject1.PNG"
+import pro2 from "../assets/myproject2.PNG"
+import pro3 from "../assets/myproject3.PNG"
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "MNTN Hiking - Landing Page",
+        text: "Explore the outdoors and live on the trail as this immersive website provides you with all the information and gear you would need to make your next hike, your best hike.",
+        view: "https://i-frostbyte.github.io/LandingPageMNTN/",
+        source: "https://github.com/I-Frostbyte?tab=repositories"
+    },
+    {
+        imgsrc: pro2,
+        title: "FreeCodeCamp - Survey Form",
+        text: "Be more than just another coder on FreeCodeCamp, express your views, both positive or negative about the site in honesty and safety and help us make it the best training engine on the planet.",
+        view: "https://i-frostbyte.github.io/Certification-Project---Survey-Form/",
+        source: "https://github.com/I-Frostbyte?tab=repositories"
+    },
+    {
+        imgsrc: pro3,
+        title: "Free Code Camp - Tribute Page",
+        text: "Learn of the man, the myth, the legend, John Ronald Reuel Tolkien and the many triumphs enjoyed and tribulations suffered on his way to creating his masterpieces: The Hobbit, The Lord of the Rings and encompassing the world of Middle-Earth in total - The Silmarillion",
+        view: "https://i-frostbyte.github.io/Certification-Project---Tribute-Page/",
+        source: "https://github.com/I-Frostbyte?tab=repositories"
+    },
+];
+
+export default ProjectCardData
