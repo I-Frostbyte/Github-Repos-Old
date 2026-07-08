39be03451f1069de49abc6fc939b7e4bbe399841
I-Frostbyte <irefyre@gmail.com>
2026-07-08 08:24:57 +0100

    Adding react-templates

---
+++ Projects/react-templates/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Hero from "./pages/Hero";
+import About from "./pages/About";
+import ContactUs from "./pages/ContactUs";
+import FAQs from "./pages/FAQs";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/faqs" element={<FAQs />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ Projects/react-templates/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'original': '#86198f',
+        'complement': '#64748b',
+        'warning': '#dc2626',
+        'success': '#22c55e',
+        // 'orange': '#ff7849',
+        // 'yellow': '#ffc82c',
+        // 'gray-dark': '#273444',
+        // 'gray': '#8492a6',
+        // 'gray-light': '#d3dce6',
+      },
+    },
+  },
+  plugins: [],
+}
+
